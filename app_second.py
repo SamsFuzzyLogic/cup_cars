bc1f639a51693a1210e60d17298942c430a0dcf2
Sam's Fuzzy Logic <165667408+SamsFuzzyLogic@users.noreply.github.com>
2025-06-30 21:26:38 -0400

    Update app_second.py

--- app_second.py
+++ app_second.py
@@ -37,7 +37,7 @@ def send_confirmation_email(to_email, name, q1, q2, q3, q4, lead_lap):
 st.set_page_config(page_title="Chicago Road Race", layout="centered")
 
 st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
-st.image("images/cupcar_logo.png", width=250)
+st.image("images/cupcar_logo.png", width=350)
 st.markdown("</div>", unsafe_allow_html=True)
 
 st.title("NASCAR Chicago Survey")
